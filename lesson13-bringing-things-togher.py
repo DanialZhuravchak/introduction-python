2b12a2b55e791ab1b2450e7dede3fe9b5aba0320
DanialZhuravchak <zhuravchak.danial@gmail.com>
2019-07-14 14:21:22 +0300

    Fixed issues, that I have before the lesson.

--- lesson13-bringing-things-togher.py
+++ lesson13-bringing-things-togher.py
@@ -2,28 +2,31 @@ game = [[2, 1, 1, ],
         [2, 2, 0, ],
         [2, 2, 0, ]]
 
+
 def win(current_game):
-#Horizontal  
+    '''Horizontal
+    '''
     for row in game:
         print(row)
         if row.count(row[0]) == len(row) and row[0] ! = 0:
             print(f"Player {row[0]} is the winner horizontally!")
 
-    #Diaginal   
+    '''Diaginal
+    '''
     diags = []
-
     for col, row in enumerate(reversed(range(len(game)))):
         diags.append(game[row][col])
-        if diags.count(diags[0]) == len(diags) and diags[0] !=0:
-            print(f"Player {row[0]} is the winner diagonaly (/)!")
+    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+        print(f"Player {diags[0]} is the winner diagonaly (/)!")
 
-    diags =[]
-
-#Vertical
+    diags = []
     for ix in range(len(game)):
         diags.append(game[ix][ix])
-        if diags.count(diags[0]) == len(diags) and diags[0] !=0:
-            print(f"Player {row[0]} is the winner diagonaly(\)!")
+    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+        print(f"Player {diags[0]} is the winner diagonaly (\\)!")
+
+    '''Vertical
+    '''
 
     for col in range(len(game)):
         check = []
@@ -32,6 +35,6 @@ def win(current_game):
             check.append(row[col])
 
         if check.count(check[0]) == len(check) and check[0] != 0:
-            print("Winner!")
+            print(f"Player {check[0]} is the winner virtically (|)!")
 
 win(game)
