f4ec50deaa71c4e7992511a55cc1ef7e279be3dd
DanialZhuravchak <zhuravchak.danial@gmail.com>
2019-07-14 15:20:21 +0300

    Finish lesson 13

--- lesson13-bringing-things-togher.py
+++ lesson13-bringing-things-togher.py
@@ -1,6 +1,8 @@
+import itertools
+
 game = [[2, 1, 1, ],
-        [2, 2, 0, ],
-        [2, 2, 0, ]]
+        [1, 2, 0, ],
+        [2, 1, 2, ]]
 
 
 def win(current_game):
@@ -8,7 +10,7 @@ def win(current_game):
     '''
     for row in game:
         print(row)
-        if row.count(row[0]) == len(row) and row[0] ! = 0:
+        if row.count(row[0]) == len(row) and row[0] != 0:
             print(f"Player {row[0]} is the winner horizontally!")
 
     '''Diaginal
@@ -27,7 +29,6 @@ def win(current_game):
 
     '''Vertical
     '''
-
     for col in range(len(game)):
         check = []
 
@@ -37,4 +38,35 @@ def win(current_game):
         if check.count(check[0]) == len(check) and check[0] != 0:
             print(f"Player {check[0]} is the winner virtically (|)!")
 
-win(game)
+
+def game_board(game_map, player=0, row=0, column=0, just_display=False):
+    try:
+        print("  0  1  2")
+        if not just_display:
+                game[row][column] = player
+        for count, row in enumerate(game):
+                print(count, row)
+        return game_map
+    except IndexError as e:
+        print("Error: make sure you input row/colomn as 0 1 or 2", e)
+
+    except Exception as e:
+        print("Something went very wrong!", e)
+
+
+play = True
+players = [1, 2]
+while play:
+    game = [[0, 0, 0, ],
+            [0, 0, 0, ],
+            [0, 0, 0, ]]
+
+    game_won = False
+    game = game_board(game, just_display=True)
+    player_choise = itertools.cycle([1, 2])
+    while not game_won:
+        current_player = next(player_choise)
+        print(f"Current Player: {current_player}")
+        column_choise = int(input("What column do you want to play? (0, 1, 2): "))
+        row_choise = int(input("What row do you want to play? (0, 1, 2): "))
+        game = game_board(game, current_player, row_choise, column_choise)
